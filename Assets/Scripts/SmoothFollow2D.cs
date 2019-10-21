d2c3db2784929365e5620ff5c325b115557696cf
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2019-10-21 16:02:01 +0100

    Unity Transfer
    Moved all assets to folde
    r

---
+++ Assets/Scripts/SmoothFollow2D.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow2D : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing = 0.5f;
+
+    // Start is called before the first frame update
+     void FixedUpdate()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPos, (smoothing *0.001f));
+    }
+}
---
+++ Assets/Scripts/TopDown2DCharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDown2DCharacterController : MonoBehaviour
+{
+
+    public float speed = 0.5f;
+    new Rigidbody2D rigidbody2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rigidbody2D.velocity = new Vector2(x, y) * speed;
+        rigidbody2D.angularVelocity = 0.0f;
+        
+    }
+}
