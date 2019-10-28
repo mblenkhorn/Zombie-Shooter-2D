7d6509fd587f85173e4d8e3ff6fd0a0fd6f0d235
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2019-10-28 15:23:36 +0000

    Created HealthSystem
    HealthSystem script coded

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class OnDamagedEvent : UnityEvent<int> { }
+
 
 public class HealthSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public int health = 10;
+    public UnityEvent onDie;
+    public OnDamagedEvent onDamaged;
+
+    public void TakeDamage (int damage)
     {
-        
+        health -= damage;
+        onDamaged.Invoke(health);
+
+        if(health < 1)
+        {
+            onDie.Invoke();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
 }
