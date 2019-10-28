cec1364d759b826df869898a8705b7b9a17c8097
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2019-10-28 15:34:03 +0000

    SmoothLookTowardsObject2D coded
    The script has functions as well as objects for the Unity inspector

---
+++ Assets/Scripts/SmoothLookTowardsTarget2D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothLookTowardsTarget2D : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing = 5.0f;
+    public float adjustmentAngle = 0.0f;
+
+    private void Update()
+    {
+        Vector3 difference = target.position - transform.position;
+
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+        Quaternion newRot = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRot, Time.deltaTime * smoothing);
+    }
+
+
+
+
+
+}
