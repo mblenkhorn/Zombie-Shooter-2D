dc103c159f9b9d0d8f43159ec1b2465c0c66a4e4
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2019-11-04 15:41:58 +0000

    Added Animation for player and explosions,
    Update Level 1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator gunAnim;
+
+    private void Start()
+    {
+        gunAnim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            print("!");
+            gunAnim.SetBool("IsFiring", true);
+        }
+        else
+        {
+            gunAnim.SetBool("IsFiring", false);
+        }
+    }
+}
