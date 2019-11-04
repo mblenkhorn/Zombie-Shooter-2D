e81e5ab73c31375bef79b54049ae2580da9cf9ef
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2019-11-04 15:04:33 +0000

    Spawner
    Spawner script coded

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject prefabToSpawn;
+    public float adjustmentAngle = 0;
 
-    // Update is called once per frame
-    void Update()
+    public void Spawn()
     {
-        
+        Vector3 rotationinDegrees = transform.eulerAngles;
+        rotationinDegrees.z += adjustmentAngle;
+
+        Quaternion rotationInRadians = Quaternion.Euler(rotationinDegrees);
+
+        Instantiate(prefabToSpawn, transform.position, rotationInRadians);
+
     }
 }
