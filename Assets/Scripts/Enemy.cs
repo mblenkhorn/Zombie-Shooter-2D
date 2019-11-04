20ebe3616b817a6cf86ab22dd348961ddac6e82c
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2019-11-04 15:48:58 +0000

    Enemy Script code
    Coded data for enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+
+[System.Serializable]
+
+public class EnemySpawnedEvent : UnityEvent<Transform> { }
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public EnemySpawnedEvent onSpawn;
+
+    private void Start()
     {
-        
+        GameObject player = GameObject.FindWithTag("Player");
+        onSpawn.Invoke(player.transform);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
 }
