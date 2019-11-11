ada91a4ed6cc9477a8b6b7acf0485f21a5da57ef
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2019-11-11 14:56:40 +0000

    GameUI Script
    Added data to the GameUI Script

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Slider healthBar;
+    public Text scoreText;
+
+    public int playerScore = 0;
+
+    private void OnEnable()
     {
-        
+        Player.OnUpdateHealth += UpdateHealthBar;
+        AddScore.OnSendScore += UpdateScore;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        Player.OnUpdateHealth -= UpdateHealthBar;
+        AddScore.OnSendScore -= UpdateScore;
     }
+
+    private void UpdateHealthBar(int health)
+    {
+        healthBar.value = health;
+    }
+
+    private void UpdateScore(int theScore)
+    {
+        playerScore += theScore;
+        scoreText.text = "Score: " + playerScore.ToString();
+    }
+
+
 }
