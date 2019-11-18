df2ee2a62dddd642c2527777386c1aa41f4a28f7
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2019-11-18 14:26:20 +0000

    Added Game Manager script
    Added data to the script and added an empty game object to contain the game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Zombie Shooter Level 1");
+    }
+    public void EndGame()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+    
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+
+
+
+
+
+
+}
