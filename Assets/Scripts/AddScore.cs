ae37975c82bf09f46d74b9c232c3c3fce7c1b57d
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2019-11-25 14:43:35 +0000

    commented out  debug
    commented out debug for the addscore script

--- Assets/Scripts/AddScore.cs
+++ Assets/Scripts/AddScore.cs
@@ -13,7 +13,7 @@ public class AddScore : MonoBehaviour
     {
         if(OnSendScore != null)
         {
-            print(score);
+            //print(score);
             OnSendScore(score);
         }
     }
