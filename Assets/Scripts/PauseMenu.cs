d5c94db1e979223316d5257062030a248065b239
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2019-12-29 21:10:21 +0000

    Updated Script
    Pause Menu script now has functions to load the menu and go back to the game.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public bool isPaused = false; 
+
+
     // Start is called before the first frame update
     void Start()
     {
+        pauseMenu = transform.GetChild(0).gameObject;
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void GamePaused()
     {
-        
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
     }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+    }
+    
 }
