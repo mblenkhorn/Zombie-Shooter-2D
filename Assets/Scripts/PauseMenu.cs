b4b3e680b98f80f46e93cebbd55007db6c171c75
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2019-12-31 22:39:36 +0000

    Updated Script
    Added commenting to explain the code and made sure the pause menu opens when the spacebar is pressed.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,42 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject pauseMenu;
-    public bool isPaused = false; 
+    public GameObject pauseMenu; //this is the pause menu panel
+    public static bool isPaused = false;  //checks to see if the game is paused
 
 
     // Start is called before the first frame update
     void Start()
     {
-        pauseMenu = transform.GetChild(0).gameObject;
+        pauseMenu = transform.GetChild(0).gameObject; //gets the pause menu panel. 
         
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) //if the spacebar is pressed
+        {
+            GamePaused(); //run this function 
+        }
     }
 
 
     public void GamePaused()
     {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0;
-        isPaused = true;
+        pauseMenu.SetActive(true); //brings up the pause menu
+        Time.timeScale = 0; //the game goes into paused mode
+        isPaused = true; //game is paused
     }
 
     public void ResumeGame()
     {
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1;
-        isPaused = false;
+        pauseMenu.SetActive(false); //removes the pause menu
+        Time.timeScale = 1; //the game goes back into play mode
+        isPaused = false; //game is not paused
 
     }
 
     public void BackToTitleScreen()
     {
-        Time.timeScale = 1;
-        SceneManager.LoadScene("Title Screen");
+        Time.timeScale = 1; //game goes back into play mode
+        SceneManager.LoadScene("Title Screen"); //loads the title screen
     }
 
     public void QuitGame()
     {
-        Application.Quit();
+        Application.Quit(); //quits the game 
         Debug.Log("Game has stopped working");
     }
     
